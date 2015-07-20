527397e9b628b9cac36370bc0f6b31fda2da87ee
Federico Roffi <fedoverseas@gmail.com>
2015-07-20 18:05:49 +0100

    Add clojurescript script example

--- casper/script/brightnorth_website_cljs.cljs
+++ casper/script/brightnorth_website_cljs.cljs
@@ -4,4 +4,16 @@
 (def url "http://www.brightnorth.co.uk/contact-us.html")
 
 
-(def casper js/casper)
+(def casper (.create (.require js/window "casper")))
+
+(.start casper "http://www.brightnorth.co.uk/"
+  (fn []
+    (this-as that
+      (.echo that (.getTitle that)))))
+
+(.thenOpen casper "http://www.brightnorth.co.uk/contact-us.html"
+  (fn []
+    (this-as that
+      (.echo that (.getTitle that)))))
+
+(.run casper)
