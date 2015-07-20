96d272c1f6de2fd103a3702942845a05cbe347a0
Federico Roffi <fedoverseas@gmail.com>
2015-07-20 17:38:26 +0100

    Created aliases

--- project.clj
+++ project.clj
@@ -24,6 +24,9 @@
 
   :uberjar-name "caspercljs.jar"
 
+  :aliases {"casper" ["do" "clean" ["with-profiles" "casper" ["cljsbuild" "test"]]]
+            "casper-test" ["do" "clean" ["with-profiles" "casper-test" ["cljsbuild" "test"]]]}
+
   :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
@@ -32,7 +35,41 @@
                                         :optimizations :none
                                         :pretty-print  true}}}}
 
-  :profiles {:dev {:source-paths ["env/dev/clj"]
+  :profiles {:casper-test {
+                           ;:source-paths ["env/dev/clj"]
+                           ;:test-paths ["test/clj"]
+
+                           ;:dependencies [[figwheel "0.2.5"]
+                           ;               [figwheel-sidecar "0.2.5"]
+                           ;               [com.cemerick/piggieback "0.1.5"]
+                           ;               [weasel "0.6.0"]]
+
+                           ;:repl-options {:init-ns caspercljs.server
+                           ;               :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+                           ;:plugins [[lein-figwheel "0.2.5"]]
+                           ;
+                           ;:figwheel {:http-server-root "public"
+                           ;           :server-port 3449
+                           ;           :css-dirs ["resources/public/css"]
+                           ;           :ring-handler caspercljs.server/http-handler}
+
+                           :env {:is-dev true}
+
+                           :cljsbuild {:test-commands {"casper" ["casperjs" "test" "resources/public/js/app_test.js"]
+                                                       ;"test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"]
+                                                       }
+                                       :builds {
+                                                ;:app {:source-paths ["env/dev/cljs"]}
+                                                :test {:source-paths [ "test/cljs"] ;; "src/cljs"
+                                                       :compiler {:output-to     "resources/public/js/app_test.js"
+                                                                  :output-dir    "resources/public/js/test"
+                                                                  :source-map    "resources/public/js/test.js.map"
+                                                                  ;:preamble      ["react/react.min.js"]
+                                                                  :optimizations :whitespace
+                                                                  :pretty-print  false}}}}}
+
+             :dev {:source-paths ["env/dev/clj"]
                    :test-paths ["test/clj"]
 
                    :dependencies [[figwheel "0.2.5"]
@@ -52,18 +89,19 @@
 
                    :env {:is-dev true}
 
-                   :cljsbuild {:test-commands {"casper" ["casperjs" "test" "resources/public/js/app_test.js"]
-                                               ;"test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"]
-                                               }
-                               :builds {
-                                        ;:app {:source-paths ["env/dev/cljs"]}
-                                        :test {:source-paths [ "test/cljs"] ;; "src/cljs"
-                                               :compiler {:output-to     "resources/public/js/app_test.js"
-                                                          :output-dir    "resources/public/js/test"
-                                                          :source-map    "resources/public/js/test.js.map"
-                                                          ;:preamble      ["react/react.min.js"]
-                                                          :optimizations :whitespace
-                                                          :pretty-print  false}}}}}
+                   ;:cljsbuild {:test-commands {"casper" ["casperjs" "test" "resources/public/js/app_test.js"]
+                   ;                            ;"test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"]
+                   ;                            }
+                   ;            :builds {
+                   ;                     ;:app {:source-paths ["env/dev/cljs"]}
+                   ;                     :test {:source-paths [ "test/cljs"] ;; "src/cljs"
+                   ;                            :compiler {:output-to     "resources/public/js/app_test.js"
+                   ;                                       :output-dir    "resources/public/js/test"
+                   ;                                       :source-map    "resources/public/js/test.js.map"
+                   ;                                       ;:preamble      ["react/react.min.js"]
+                   ;                                       :optimizations :whitespace
+                   ;                                       :pretty-print  false}}}}
+                   }
 
              :uberjar {:source-paths ["env/prod/clj"]
                        :hooks [leiningen.cljsbuild]
